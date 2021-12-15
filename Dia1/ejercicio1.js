ff7655ef1ae7c3feccf91ea519cbbbacd3f75deb
José Leyder Gil López <31496801+leydergil@users.noreply.github.com>
2021-12-15 15:35:53 +0000

    Initial commit

---
+++ Dia1/ejercicio1.js
@@ -0,0 +1,4 @@
+let msg = prompt('Hola Maker, Ingresa un Mensaje por favor :');
+console.log(msg);
+
+console.log(`La frase ingresada es ${msg}`);
---
+++ Dia1/ejercicio3.js
@@ -0,0 +1,5 @@
+let primerNumero = prompt("Ingrese el primer número: ");
+let segundoNumero = prompt("Ingrese el segundo número: ");
+
+let suma = parseFloat(primerNumero) + parseFloat(segundoNumero);
+console.log(`El resultado de la suma es : ${suma}`);
