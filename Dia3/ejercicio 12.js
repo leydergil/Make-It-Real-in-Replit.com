778dd3b3051ec04f4f93412b57ed019d21f87235
José Leyder Gil López <31496801+leydergil@users.noreply.github.com>
2021-12-17 16:02:48 +0000

    Ejercicio 12

--- Dia3/ejercicio 12.js
+++ Dia3/ejercicio 12.js
@@ -10,10 +10,11 @@ const num = argumentos[2];
 let arreglo = [];
 
 for(let index=1; index<= num; index++){
+  //Imprimir los números de acuerdo al n´´umero ingresado
   console.log(index);
 
+  //Probando a poner los elementos en un arreglo
   arreglo.push(index);
 } 
 console.log(arreglo);
 console.log(arreglo.length)
-
